cc7e6943584774dbbe7b550650b854bbabb72bdb
unknown <guilhermedealmeidasilva17@gmail.com>
2024-05-10 14:08:58 -0300

    finalizado com sucesso

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿int glicemia;
+string diagnostico;
+ConsoleColor CorResultado;
+
+Console.WriteLine(" ||| Diabetes Gestacional |||\n");
+
+Console.Write("Glicemia (em mg/dL) =>");
+glicemia = Convert.ToInt32(Console.ReadLine());
+
+if (glicemia > 126)
+{
+    diagnostico = "- Diabetes Mellitus na gravidez -";
+    CorResultado = ConsoleColor.Red;
+}
+else
+{
+    if (glicemia >= 92)
+    {
+        diagnostico = "- Compatível com Diabetes Gestacional -";
+        CorResultado = ConsoleColor.Yellow;
+    }
+    else
+    {
+        diagnostico = "- Reavaliar entre 24 e 28 semanas com TTOG 75g de glicose -";
+        CorResultado = ConsoleColor.Green;
+    }
+}
+
+Console.ForegroundColor = CorResultado;
+
+Console.WriteLine();
+Console.WriteLine(diagnostico);
+
+Console.ResetColor();
